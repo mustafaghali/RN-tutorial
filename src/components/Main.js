7f5e56425ad368dad6f6f7196d420834984ee41f
mustafaghali <wad_alghali@hotmail.com>
2018-07-26 16:36:11 +0200

    blinkApp

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,17 +1,18 @@
 import React, { Component } from 'react';
 import {View } from 'react-native';
-import GreetingText from './common/Greeting.js'
+import Blink from './common/Blink.js'
 
 
 
-export default class LotsOfGreetings extends Component {
+export default class Main extends Component {
   render() {
     return (
-      <View style={{alignItems: 'center'}}>
-        <GreetingText name='Vodafone' />
-        <GreetingText name='Mustafa' />
-        <GreetingText name='Moh' /> 
+      <View style =  {{flex:1, alignItems:'center',justifyContent:'center', backgroundColor:'#e60000'}}> 
+        <Blink text='I love to blink'  TextColor = 'white' />
+        <Blink text='Yes blinking is so great' />
+        <Blink text='Why did they ever take this out of HTML' />
+        <Blink text='Look at me look at me look at me' />
       </View>
     );
   }
-};
+}

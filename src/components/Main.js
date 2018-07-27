5aa23d653e1482adb8f1c51df5f814c970fb0f21
mustafaghali <wad_alghali@hotmail.com>
2018-07-27 18:27:13 +0200

    networking

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,63 +1,134 @@
-import React, {Component} from 'react';
-import {Modal, Text, TouchableOpacity, View} from 'react-native';
+import React, { Component } from 'react';
+import {FlatList, 
+  StyleSheet, 
+  Text, 
+  View,
+  Dimensions,
+  ActivityIndicator, 
+  NetInfo} from 'react-native';
+import axios from 'axios'; 
 
-export default class Main extends Component {
-  state = {
-    modalVisible: false,
-  };
+const { width } = Dimensions.get('window');
 
-  setModalVisible(visible) {
-    this.setState({modalVisible: visible});
+
+function MiniOfflineSign() {
+  return (
+    <View style={styles.offlineContainer}>
+      <Text style={styles.offlineText}>No Internet Connection !</Text>
+    </View>
+  );
+}
+
+
+
+
+export default class FlatListBasics extends Component {
+  constructor() {
+    super();
+    this.state = {
+      loading:true,
+      names : [],
+      isConnectedToInternet:false    
+    };
+}
+  
+handleConnectivityChange = isConnected => {
+    this.setState({isConnectedToInternet:isConnected});
+};
+
+
+componentDidMount() {
+  NetInfo.isConnected.addEventListener('connectionChange', this.handleConnectivityChange);
+}
+
+componentWillUnmount() {
+NetInfo.isConnected.removeEventListener('connectionChange', this.handleConnectivityChange);
+}
+
+
+
+  componentWillMount(){
+    axios.get('https://jsonplaceholder.typicode.com/posts')
+    .then((response)=> {
+      // handle success
+      console.log(response);
+      this.setState({ names :response.data.map((item)=>{return {...item,key:item.title}})}); 
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    })
+    .then( ()=> {  
+      this.setState({ loading:false});
+     
+      // always executed
+    });
+  }
+  
+
+  
+  OfflineMessage() {
+    if (!this.state.isConnectedToInternet)
+    return <MiniOfflineSign />;
+    else 
+    return null;
   }
 
   render() {
+  
     return (
-      <View style={{flex:1,marginTop: 22,alignItems:'center',justifyContent:'center'}}>
-        <Modal
-          animationType="slide" 
-          transparent={true}
-          visible={this.state.modalVisible}
-          onRequestClose={() => {
-            alert('Modal has been closed.');  
-          }}>
-          <View style={{marginTop: 22,width:200,height:200,backgroundColor:'rgba(227,111,158,0.7)',alignItems:'center',justifyContent:'center',borderRadius:20}}>   
-              <Text>Hello World!</Text> 
- 
-              <TouchableOpacity
-                style = {{...styles.buttonStyle,borderColor:'white'}}
-                onPress={() => {
-                  this.setModalVisible(!this.state.modalVisible);
-                }}>
-                <Text>Hide Modal</Text>
-              </TouchableOpacity>
-          </View>
-        </Modal>
+      <View style={styles.container}>
 
-        <TouchableOpacity
-        style = {styles.buttonStyle}
-          onPress={() => {
-            this.setModalVisible(true); 
-          }}>
-             <Text>Show Modal</Text> 
-        </TouchableOpacity>
+       {this.OfflineMessage()}
+
+        {this.state.loading?  
+          <View>
+            <Text>loading...</Text>
+            <ActivityIndicator
+              //  animating = {animating}
+               color = '#bc2b78'
+               size = "large"
+               style = {styles.activityIndicator}/>
+          </View>
+          :
+          <FlatList
+            data={this.state.names}
+            renderItem={
+            ({item}) => 
+            <View style = {{width:Dimensions.get('window').width*0.5}}> 
+             <Text style={styles.item}>{item.title}</Text>
+             <Text style = {styles.item}>{item.body}</Text>
+             <View style = {{borderBottomColor:'black',borderBottomWidth:1}} />
+             </View>
+             }
+          /> 
+         }
       </View>
+
     );
   }
 }
-
-const styles = {
-    buttonStyle : {
-    width:160 ,
-    height:30,
-    alignItems:'center',
-    justifyContent:'center', 
-    borderWidth:1,
-    borderColor:'gray',
-    borderRadius:3,
-    shadowColor:'black',
-    shadowOffset:{width : 0 , height : 2},
-    shadowOpacity:0.1,
-    shadowRadius:2,
-    elevation:1
+const styles = StyleSheet.create({
+  container: {
+   flex: 1,
+   paddingTop: 22
+  },
+  item: {
+    padding: 10,
+    fontSize: 18,
+    height: 44,
+  },
+  offlineContainer: {
+    backgroundColor: 'rgb(181,36,36)',
+    height: 30,
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'row',
+    width,
+    position: 'absolute',
+    //top: 30
+  },
+  offlineText: { 
+    color: '#fff'
   }
-}
+})

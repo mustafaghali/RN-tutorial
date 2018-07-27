34c43d28fc59ac50f02a2c01d30a3e2ae114890a
mustafaghali <wad_alghali@hotmail.com>
2018-07-28 00:06:40 +0200

    loginScreen

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 
-import Main from './src/components/Main.js'
+import Login from './src/components/Login.js'
 
 export default class App extends Component {
   render() {
     return ( 
-       <Main/>
+       <Login/>
      );
    }
   };
